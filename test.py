4b7060a1c7243f234d82de6b432473ff5cb2967f
thecoldguy <162477898+thecoldguy@users.noreply.github.com>
2024-09-23 10:42:22 +0545

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+# Get two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = num1  num2
+
+# Print the result
+print(f"The result of multiplying {num1} and {num2} is: {result}")
